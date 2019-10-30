78e7c24f443ca0791427d556892030d47d208e22
daniilisaew <57032484+daniilisaew@users.noreply.github.com>
2019-10-30 10:39:05 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,5 @@
 <?php 
 	include 'conect.php';
-
  ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,8 +17,10 @@
 		<div class="row">
 			<form action="submit.php" method="POST" class="form-group">
 				
-			<input type="text" placeholder="Login..." name="login" required>
-			<input type="text" placeholder="Password..." name="pass" required>
+		<input type="text" placeholder="Login..." name="login" required pattern="[A-Za-z0-9]+" min="6" max="18">
+		<label for="">Enter min 6 max 18 symbol</label>
+		<input type="text" placeholder="Password..." name="pass" required pattern="[A-Za-z0-9]+" min="6" max="12">
+		<label for="">Enter min 6 max 12 symbol</label>
 			<button type="submit" class="btn btn-primary btn-round">Submit</button>
 		</div>
 	</div>
